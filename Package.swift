3ad1b81be7d31f003e558c4dc69b7e22244fbdc5
Lorne <www@keymic.io>
2026-05-03 22:06:56 +1000

    build: update build config and package manifest

--- Package.swift
+++ Package.swift
@@ -4,9 +4,18 @@ import PackageDescription
 let package = Package(
     name: "KeyMic",
     platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(
+            url: "https://github.com/sparkle-project/Sparkle",
+            "2.6.0"..<"3.0.0"
+        )
+    ],
     targets: [
         .executableTarget(
             name: "KeyMic",
+            dependencies: [
+                .product(name: "Sparkle", package: "Sparkle")
+            ],
             path: "Sources/KeyMic"
         )
     ]
