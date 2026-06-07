cfc55c466fc1ad8879cf84bad73766d1d112c044
lorne-luo <lorne-luo@users.noreply.github.com>
2026-06-08 00:10:12 +0800

    feat(onnx): add CSherpaOnnx C target scaffold

---
+++ Sources/CSherpaOnnx/SherpaBridge.c
@@ -0,0 +1,2 @@
+#include "SherpaBridge.h"
+int sherpa_bridge_placeholder(void){return 0;}
---
+++ Sources/CSherpaOnnx/include/SherpaBridge.h
@@ -0,0 +1,4 @@
+#ifndef SHERPA_BRIDGE_H
+#define SHERPA_BRIDGE_H
+int sherpa_bridge_placeholder(void);
+#endif
